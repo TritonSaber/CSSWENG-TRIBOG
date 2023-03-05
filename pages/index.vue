1521120f6450f07a610fec0ac5f05dfd5dbbb50d
Eugene Sillona <93899090+jojiSillona@users.noreply.github.com>
2023-03-05 23:28:57 +0800

    Basic HTML and CSS

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,33 @@
 <template>
-  <div class="">
-    <h1>Tribog Client</h1>
+  <div class="index">
+    <div id="logo">LOGO</div>
+    <div id="carousel">CAROUSEL</div>
+    <div id="nav">
+      <div class="nav-button">
+        <div class="nav-button-content">
+          <!-- IMAGE HERE -->
+          TO BE DELIVERED
+        </div>
+      </div>
+      <div class="nav-button">
+        <div class="nav-button-content">
+          <!-- IMAGE HERE -->
+          CONTACTS
+        </div>
+      </div>
+      <div class="nav-button">
+        <div class="nav-button-content">
+          <!-- IMAGE HERE -->
+          INVENTORY TRACKER
+        </div>
+      </div>
+      <div class="nav-button">
+        <div class="nav-button-content">
+          <!-- IMAGE HERE -->
+          TRANSACTION HISTORY
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,4 +35,27 @@
 </script>
 
 <style lang="scss">
+  .index{
+    width: 100vw;
+    height: 100vh;
+
+    background-color: #d9d9d9;
+  }
+
+  #nav{
+    display: flex;
+    justify-content: center;
+  }
+
+  .nav-button{
+    display: inline-block;
+    margin-inline: 18px;
+
+    width: 258px;
+    height: 175px;
+
+    background-color: white;
+
+    box-shadow: 0px 4px 4px;
+  }
 </style>
