cdd6a81c4bf7199170602e5a7c423e7336a9ac09
Eugene Sillona <93899090+jojiSillona@users.noreply.github.com>
2023-03-29 23:36:19 +0800

    Added Contacts with basic Styling

---
+++ pages/contacts.vue
@@ -0,0 +1,38 @@
+<template>
+    <CommonContainer>
+        <CommonHeadingDetails />
+        <div class="font-weight-bold">CONTACTS</div>
+        <b-container class="mt-4 d-flex justify-content-center">
+            <b-row align-v="center">
+                <b-col class="d-flex flex-column align-items-center">
+                    <img src="../assets/images/main-office.png" alt="main-office" class="mb-4">
+                    <span class="font-weight-bold">Main Office</span>
+                    <p class="text-center">
+                        Lezyl Egenias <br/>
+                        +63 915 850 5143 <br/>
+                    </p>
+                    <p class="text-center">
+                        Gina Perez <br/>
+                        +63 915 850 5143 <br/>
+                    </p>
+                </b-col >
+                <b-col class="d-flex flex-column align-items-center">
+                    <img src="../assets/images/store.png" alt="store" class="mb-4">
+                    <span class="font-weight-bold">Store</span>
+                    <p class="text-center">
+                        Nora<br/>
+                        +63 927 733 6763<br/>
+                    </p>
+                    <p class="text-center">
+                        Jane<br/>
+                        +63 929 535 0308<br/>
+                    </p>
+                </b-col>
+            </b-row>
+        </b-container>
+    </CommonContainer>
+</template>
+
+<script>
+export default {}
+</script>
