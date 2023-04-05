e8c3eebad835b94b81be00c676f2a6bb474763e3
Eugene Sillona <93899090+jojiSillona@users.noreply.github.com>
2023-04-05 23:24:54 +0800

    FEATURE - Added Transactions Front-end and Funcs

---
+++ pages/transactions/EditTransactionsTable/EditTransactionsTable.vue
@@ -0,0 +1,163 @@
+<template>
+  <b-modal
+    id="edit-transaction-modal"
+    centered
+    title="Edit Transaction"
+    modal-class="generic-modal"
+  >
+    <b-form class="generic-form">
+      <b-form-group>
+        <b-row>
+          <b-col>
+            <CommonProductSelect
+              type="product"
+              v-on:productChanges="$event => setProduct($event)"
+            />
+          </b-col>
+          <b-col>
+            <label for="quantity">Quantity <span>*</span></label>
+            <b-form-input
+              id="quantity"
+              type="number"
+              min="0"
+              placeholder="Enter Quantity"
+              required
+              v-model="formData.quantity"
+              @keydown="preventDecimal"
+              @blur="setQuantityToZero"
+            ></b-form-input>
+          </b-col>
+        </b-row>
+      </b-form-group>
+
+      <b-form-group>
+        <b-row>
+          <b-col>
+            <label for="total_cost">Total Cost <span>*</span></label>
+            <b-form-input
+              id="price"
+              type="number"
+              step="0.01"
+              min="0"
+              placeholder="Enter Total Cost"
+              required
+              v-model="formData.total_cost"
+              @blur="setTotalCostToZero"
+            ></b-form-input>
+          </b-col>
+        </b-row>
+      </b-form-group>
+    </b-form>
+
+    <template #modal-footer="{ cancel }">
+      <b-button @click="cancel()"> Cancel </b-button>
+      <b-button
+        type="submit"
+        class="btn-info"
+        @click="submit"
+        :disabled="submitting || !enableSubmit"
+        >Add</b-button
+      >
+    </template>
+  </b-modal>
+</template>
+
+<script>
+import Helpers from '~/mixins/Helpers'
+
+export default {
+  mixins: [Helpers],
+
+  props: {
+    transaction: {
+      type: Object | null,
+      required: true,
+    }
+  },
+
+  data() {
+    return {
+      submitting: false,
+
+      formData: {
+        quantity: 0,
+        total_cost: 0,
+      },
+    }
+  },
+
+  methods: {
+    async submit() {
+      this.submitting = true
+
+      try {
+        await this.$axios.$post(`${process.env.baseUrl}/transactions-update/${this.transaction.id}`, this.requestParams)
+      } catch (e) {
+        console.log(e)
+      } finally {
+        this.submitting = false
+
+        this.$bvModal.hide('edit-transaction-modal')
+
+        this.$nuxt.$emit('editedTransaction', true)
+
+        this.resetFormData()
+      }
+    },
+
+    setQuantityToZero() {
+      if (!this.formData.quantity) {
+        this.formData.quantity = 0;
+      }
+    },
+
+    setTotalCostToZero() {
+      if (!this.formData.cost) {
+        this.formData.cost = this.numberFormat(0);
+      }
+    },
+
+    resetFormData() {
+      this.formData = {
+        quantity: 0,
+        total_cost: this.numberFormat(0),
+      }
+    }
+  },
+
+  computed: {
+    enableSubmit() {
+      return (
+        this.quantityParams &&
+        this.totalCostParams
+      )
+    },
+
+    quantityParams() {
+      return (this.formData.quantity >= 0)
+    },
+
+    totalCostParams() {
+      return (this.formData.cost >= 0)
+    },
+
+    requestParams() {
+      return {
+        ...this.formData,
+        quantity: Number(this.formData.quantity),
+        total_cost: Number(this.numberFormat(this.formData.cost)),
+      }
+    },
+  },
+
+  watch: {
+    transaction(newValue) {
+      if (newValue) {
+        const { product_id, quantity, total_cost} = newValue
+
+        this.formData = { product_id, quantity, total_cost }
+      }
+    }
+  }
+}
+</script>
---
+++ pages/transactions/TransactionsTable/TransactionsTable.vue
@@ -0,0 +1,302 @@
+<template>
+  <div>
+    <b-table
+      responsive
+      class="btable__container mb-0"
+      :fields="fields"
+      :items="products"
+      :show-empty="!loading"
+      :busy="isBusy"
+      empty-text="There are no transactions found!"
+      sort-icon-left
+      ref="paginatedTable"
+    >
+    <!-- add new tag in id -->
+    <template #cell(id)="row">
+      <div 
+        class="d-flex align-items-center"
+        :class="[
+          row.item.isNew
+            ? 'justify-content-between'
+            : 'justify-content-end',
+        ]"
+      >
+        <div v-if="row.item.isNew" class="tag new mr-2">NEW</div>
+        {{ row.item.id }}
+      </div>
+    </template>
+
+    <!-- format cost -->
+    <template #cell(total_cost)="row">
+      PHP {{ numberFormat(row.item.total_cost) }}
+    </template>
+
+    <!-- format date -->
+    <template #cell(created_at)="row">
+      {{ row.item.created_at | formatDate }}
+    </template>
+    <template #cell(updated_at)="row">
+      {{ row.item.updated_at | formatDate }}
+    </template>
+
+    <!-- actions button -->
+    <template #cell(actions)="row">
+      <div class="d-flex align-items-center justify-content-center bg-white">
+        <CommonActionButton
+          icon="edit bg-white"
+          v-on:click="$event => showEditTransactionsModal(row.item)"
+        />
+        <CommonActionButton
+          icon="trash bg-white"
+          childClass="danger ml-3"
+          v-on:click="$event => showDeleteTransactionsModal(row.item)"
+        />
+      </div>
+    </template>
+  </b-table>
+
+  <CommonPagination 
+    :currentPageCount="currentPageCount"
+    :totalRows="totalRows"
+    :perPage="limitPerPage"
+    @pageChange="fetchPage"
+  />
+
+  <EditTransactionsTable :product="selectedProduct" />
+
+  <CommonDeleteModal 
+    type="Transaction"
+    :isDeleting="isDeleting"
+    v-on:click="deleteTransaction"
+  />
+  </div>
+  
+</template>
+
+<script>
+import EditTransactionsTable from '../EditTransactionsTable/EditTransactionsTable'
+import _ from 'lodash'
+import Helpers from '~/mixins/Helpers'
+
+export default {
+  components: { EditTransactionsTable },
+
+  mixins: [Helpers],
+
+  props: {
+    search: {
+      type: String,
+      default: '',
+    }
+  },
+
+  data() {
+    return {
+      data: [],
+
+      isBusy: false,
+      loading: true,
+      hasError: false,
+      hasMore: true,
+
+      limitPerPage: 25,
+      currentPageCount: 0,
+      currentPage: 1,
+      totalRows: 0,
+
+      fields: [
+        { 
+          label: 'Transaction ID',
+          key: 'id', 
+          sortable: true, 
+          thClass: 'thead',
+          tdClass: 'tbody',
+        },
+        { 
+          label: 'Product ID',
+          key: 'product_id', 
+          sortable: true, 
+          thClass: 'thead',
+          tdClass: 'tbody',
+        },
+        {
+          label: 'Product Name',
+          key: 'product_name', 
+          sortable: true,
+          thClass: 'thead',
+          tdClass: 'tbody text-center',
+        },
+        {
+          label: 'Quantity',
+          key: 'quantity', 
+          sortable: true,
+          thClass: 'thead',
+          tdClass: 'tbody text-right',
+        },
+        {
+          label: 'Total Cost',
+          key: 'total_cost', 
+          sortable: true,
+          thClass: 'thead',
+          tdClass: 'tbody text-right',
+        },
+        {
+          label: 'Created At',
+          key: 'created_at', 
+          sortable: true,
+          thClass: 'thead',
+          tdClass: 'tbody',
+        },
+        {
+          label: 'Updated At',
+          key: 'updated_at', 
+          sortable: true,
+          thClass: 'thead',
+          tdClass: 'tbody',
+        },
+        {
+          label: 'Actions',
+          key: 'actions', 
+          thClass: 'thead',
+          tdClass: 'tbody',
+        },
+      ],
+
+      selectedProduct: null,
+      isDeleting: false,
+      isAdding: false,
+      isEditing: false,
+    }
+  },
+
+  methods: {
+    async toggleBusy() {
+      this.isBusy = !this.isBusy
+    },
+
+    async getTransactions() {
+      this.toggleBusy()
+
+      try {
+        const response = await this.$axios.$get(`${process.env.baseUrl}/transactions-collection`, {
+          params: {
+            keyword: this.searchParams,
+            page: this.currentPage,
+          }
+        })
+
+        this.currentPageCount = response.meta.to
+        this.totalRows = response.meta.total
+
+        this.data = response.data
+        this.loading = false
+      } catch (e) {
+        console.log(e)
+
+        this.loading = false
+        this.hasError = true
+      } finally {
+        this.toggleBusy()
+        this.loading = false
+      }
+    },
+
+    async fetchPage(page) {
+      this.currentPage = page
+      await this.getTransactions()
+    },
+
+    showEditTransactionsModal(transaction) {
+      this.selectedTransaction= transaction
+
+      this.$bvModal.show('edit-transaction-modal')
+    },
+
+    showDeleteTransactionsModal(transaction) {
+      this.selectedTransaction = transaction
+
+      this.$bvModal.show('delete-modal')
+    },
+
+    async deleteTransaction() {
+      this.isDeleting = true
+
+      try {
+        await this.$axios.$delete(`${process.env.baseUrl}/transactions-delete/${this.selectedTransaction?.id}`)
+
+        this.selectedTransaction = null
+      } catch (e) {
+        console.log(e)
+      } finally {
+        this.$bvModal.hide('delete-modal')
+        this.isDeleting = false
+
+        this.getTransactions()
+      }
+    }
+  },
+
+  computed: {
+    products() {
+      const transactions = this.data.map((transaction) => ({
+        ...transaction,
+        isNew: this.isNew(transaction.created_at),
+      }))
+
+      return _.sortBy(transactions, ['product_name'])
+    },
+
+    searchParams() {
+      return this.search
+    },
+  },
+
+  created() {
+    this.$nuxt.$on('addedTransaction', (isAdding) => {
+      this.isAdding = isAdding
+    })
+
+    this.$nuxt.$on('editedTransaction', (isEditing) => {
+      this.isEditing = isEditing
+    })
+  },
+
+  watch: {
+    search() {
+      this.getTransactions()
+    },
+
+    isAdding() {
+      this.getTransactions()
+      this.isAdding = false
+    },
+
+    isEditing() {
+      this.getTransactions()
+      this.isEditing = false
+    }
+  },
+
+  mounted() {
+    this.getTransactions()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.tag.new {
+  color: white;
+  background-color: green;
+  padding: 4px 10px;
+  font-size: 8px;
+  border-radius: 5px;
+  letter-spacing: 1px;
+  font-weight: 500;
+}
+
+.product-description {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+</style>
